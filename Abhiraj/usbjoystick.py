af23a5989e9cec2be7a670b191dd7f673a61eb73
Abhiraj Tiwari <abhirajtiwari@gmail.com>
2019-01-25 19:31:14 +0530

    minor bug fix

--- Abhiraj/usbjoystick.py
+++ Abhiraj/usbjoystick.py
@@ -10,12 +10,13 @@ try:
     while True:
         for event in pygame.event.get():
             if event.type == pygame.JOYAXISMOTION:
-                if event.axis == 3:
-                    x_joy = event.value*-255 #-255 because the joystick was reverse mapped
-                elif event.axis == 2:
-                    y_joy = event.value*255
+                if event.axis == 0:
+                    x_joy = event.value*255 #-255 because the joystick was reverse mapped
+                elif event.axis == 1:
+                    y_joy = event.value*-255
+        # print x_joy, y_joy
         lpwm = x_joy+y_joy
-        rpwm = x_joy-y_joy
+        rpwm = y_joy-x_joy
         lpwm = max(min(255, lpwm), -255)
         rpwm = max(min(255, rpwm), -255)
         print lpwm, rpwm
