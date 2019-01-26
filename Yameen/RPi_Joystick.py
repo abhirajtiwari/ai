8ecd6853b6eacd75a183008c81ec5f401f9d0ae3
nemaye <yameen.abdullah.khan@gmail.com>
2019-01-26 18:59:13 +0530

    upgraded

--- Yameen/RPi_Joystick.py
+++ Yameen/RPi_Joystick.py
@@ -4,45 +4,56 @@ import pygame
 pygame.init()
 pygame.joystick.init()
 
-for event in pygame.event.get():
-    if(event.type == pygame.JOYAXISMOTION):
-        if(event.axis==0):
-            x_value = pygame.joystick.Joystick.get_axis(0)  #gets the position of the joystick on x axis
-        elif(event.axis==1):
-            y_value = pygame.joystick.Joystick.get_axis(1)
+joyStick = pygame.joystick.Joystick(0)
+joyStick.init()
 
-    else:
-        print('not on the axis')
+x_value = 0.00
+y_value = 0.00
 
+while True:
+    for event in pygame.event.get():
+        if(event.type == pygame.JOYAXISMOTION):
+            if(event.axis==0):
+                x_value = joyStick.get_axis(0)  #gets the position of the joystick on x axis
+            elif(event.axis==1):
+                y_value = -joyStick.get_axis(1)
 
-    print('x_value',x_value)
-    print('y_value',y_value)
-
-
-#quad 1
-    if(x_value > 0 and y_value > 0):
-        if(x_value - y_value < 0):
-            print('oct 1')
         else:
-            print('oct 2')
-#quad 3
-    if(x_value < 0 and y_value < 0):
-        if(x_value - y_value < 0):
-            print('oct 5')
-        else:
-            print('oct 6')
-#quad 2
-    if(x_value < 0 and y_value > 0):
-        if(x_value + y_value > 0):
-            print('oct 3')
-        else:
-            print('oct 4')
-#quad 4
-    if(x_value > 0 and y_value < 0):
-        if(x_value + y_value > 0):
-            print('oct 8')
-        else:
-            print('oct 7')
+            print('not on the axis')
+
+
+        print('x_value',x_value)
+        print('y_value',y_value)
+
+
+    	if(x_value == 0 and y_value ==0):
+	    print('center')
+
+
+    #quad 1
+        if(x_value > 0 and y_value > 0):
+            if(x_value - y_value < 0):
+                print('oct 1')
+            else:
+                print('oct 2')
+    #quad 3
+        if(x_value < 0 and y_value < 0):
+            if(x_value - y_value < 0):
+                print('oct 5')
+            else:
+                print('oct 6')
+    #quad 2
+        if(x_value < 0 and y_value > 0):
+            if(x_value + y_value > 0):
+                print('oct 3')
+            else:
+                print('oct 4')
+    #quad 4
+        if(x_value > 0 and y_value < 0):
+            if(x_value + y_value > 0):
+                print('oct 8')
+            else:
+                print('oct 7')
 
 
 
