555e6d0a5fbea35eb11bf238a2f48b2a9f880fca
Abhiraj Tiwari <abhirajtiwari@gmail.com>
2019-03-02 21:05:36 +0530

    no one can import client

--- Abhiraj/socket_camera/client.py
+++ Abhiraj/socket_camera/client.py
@@ -5,31 +5,65 @@ import socket
 # socket.setdefaulttimeout(0.033)
 
 port = 1234
-# server = '10.42.0.15'
-server = '127.0.0.1'
+# server = '127.0.0.1'
+server = '10.42.0.15'
+factor = 2
+l = int(640/factor)
+b = int(480/factor)
+ch = 3
+size = l*b*ch
 
-while True:
+
+if __name__ == '__main__':
+    while True:
+        try:
+            data = ''
+            sock = socket.socket()
+            sock.connect((server, port))
+            while len(data) < size:
+                data += sock.recv(int(1e7))
+            print len(data)
+            # data = sock.makefile().readline()
+            frame = np.fromstring(data, dtype='uint8')
+            # frame = np.hstack((frame, np.zeros(921600-len(frame)))) 
+            # print frame
+            if frame.shape[0] != size:
+                print 'a'
+                sock.close()
+                continue
+            frame = np.reshape(frame, (b, l, ch), 'F')
+            frame = cv.resize(frame, (int(l*factor), int(b*factor)), interpolation=cv.INTER_LINEAR)
+            cv.imshow('frame', frame)
+            key = cv.waitKey(1)
+            if key == ord('q'):
+                break
+            sock.close()
+        except socket.timeout:
+            pass
+
+def getCameraFrame():
     try:
+        data = ''
         sock = socket.socket()
         sock.connect((server, port))
-        data1 = sock.recv(int(1e7))
-        data2 = sock.recv(int(1e7))
-        data3 = sock.recv(int(1e7))
-        data = data1+data2+data3
-        # print len(data)
+        while len(data) < size:
+            data += sock.recv(int(1e7))
+        print len(data)
         # data = sock.makefile().readline()
         frame = np.fromstring(data, dtype='uint8')
         # frame = np.hstack((frame, np.zeros(921600-len(frame)))) 
         # print frame
-        if frame.shape[0] != 480*640*3:
+        if frame.shape[0] != size:
             print 'a'
             sock.close()
-            continue
-        frame = np.reshape(frame, (480, 640, 3), 'F')
-        cv.imshow('frame', frame)
-        key = cv.waitKey(1)
-        if key == ord('q'):
-            break
+            return
+        frame = np.reshape(frame, (b, l, ch), 'F')
+        frame = cv.resize(frame, (int(l*factor), int(b*factor)), interpolation=cv.INTER_LINEAR)
+        return frame
+        # cv.imshow('frame', frame)
+        # key = cv.waitKey(1)
+        # if key == ord('q'):
+        #     break
         sock.close()
     except socket.timeout:
         pass
