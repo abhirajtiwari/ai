90f2eedd47e8148657ed65e655875aec54bc5784
Abhiraj Tiwari <abhirajtiwari@gmail.com>
2019-03-03 16:33:02 +0530

    jpeg encoding

---
+++ Abhiraj/socket_camera/client_encoded.py
@@ -0,0 +1,81 @@
+import cv2 as cv
+import numpy as np
+import socket
+
+# socket.setdefaulttimeout(0.033)
+
+port = 1234
+server = '127.0.0.1'
+# server = '10.57.0.62'
+# server = '192.168.43.61'
+# factor = 4
+# l = int(640/factor)
+# b = int(480/factor)
+# ch = 3
+# size = l*b*ch
+
+def getCameraFrame():
+    try:
+        data = ''
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((server, port))
+        size = int(sock.recv(7))
+        while len(data) < size:
+            data += sock.recv(int(1e7))
+            # print len(data)
+        sock.sendall('d')
+        # data = sock.makefile().readline()
+        frame_encoded = np.fromstring(data, dtype='uint8')
+        frame_encoded =  np.reshape(frame_encoded, (len(frame_encoded), 1))
+        # frame = np.hstack((frame, np.zeros(921600-len(frame)))) 
+        # print frame
+        frame = cv.imdecode(frame_encoded, -1)
+        # if frame.shape[0] != size:
+        #     print 'a'
+        #     sock.close()
+        #     return
+        # frame = np.reshape(frame, (b, l, ch), 'F')
+        # frame = cv.resize(frame, (int(l*factor), int(b*factor)), interpolation=cv.INTER_LINEAR)
+        # return frame
+        # cv.imshow('frame', frame)
+        # key = cv.waitKey(1)
+        # if key == ord('q'):
+        #     break
+        sock.close()
+        return frame
+    except socket.timeout:
+        pass
+
+
+if __name__ == '__main__':
+    while True:
+        # try:
+        #     data = ''
+        #     sock = socket.socket()
+        #     sock.connect((server, port))
+        #     while len(data) < size:
+        #         data += sock.recv(int(1e7))
+        #     print len(data)
+        #     # data = sock.makefile().readline()
+        #     frame = np.fromstring(data, dtype='uint8')
+        #     # frame = np.hstack((frame, np.zeros(921600-len(frame)))) 
+        #     # print frame
+        #     if frame.shape[0] != size:
+        #         print 'a'
+        #         sock.close()
+        #         continue
+        #     frame = np.reshape(frame, (b, l, ch), 'F')
+        #     frame = cv.resize(frame, (int(l*factor), int(b*factor)), interpolation=cv.INTER_LINEAR)
+        #     cv.imshow('frame', frame)
+        #     key = cv.waitKey(1)
+        #     if key == ord('q'):
+        #         break
+        #     sock.close()
+        # except socket.timeout:
+        #     pass
+        cv.imshow('frame', getCameraFrame())
+        key = cv.waitKey(1)
+        if key == ord('q'):
+            break
+
+
---
+++ Abhiraj/socket_camera/server_encoded.py
@@ -0,0 +1,60 @@
+import cv2 as cv
+import time
+import numpy as np
+import socket
+
+cap = cv.VideoCapture(0)
+# fps = int(cap.get(cv.CAP_PROP_FPS))
+fps = 30
+
+#socket.setdefaulttimeout(0.100)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# sock.setblocking(0)
+
+host = socket.gethostname()
+
+port = 1234
+factor = 2
+
+sock.bind(('', port))
+
+sock.listen(5)
+
+#c, addr = sock.accept()
+
+while True:
+    _, frame = cap.read()
+    _, encoded = cv.imencode('.jpg', frame)
+    length = len(encoded)
+    string_encoded = encoded.tostring()
+    # b, l, ch = frame.shape 
+    # frame = cv.resize(frame, (int(l/factor), int(b/factor)))
+    if frame is not None:
+        try:
+            # data =frame.flatten('F').tostring()
+            # data = np.fromstring(data, dtype='uint8')
+            # data = np.reshape(data, (480, 640, 3))
+            # cv.imshow('frame', frame)
+            # key = cv.waitKey(1)
+            # if key == ord('q'):
+            #     break
+            # print "here also"
+            c, addr = sock.accept()
+            # print "here"
+            # c.send(str(len(data)))
+            # c.sendall(data)
+            c.sendall(format(length, '07')+string_encoded)
+            rec = c.recv(1)
+            while rec != 'd':
+                continue
+            # time.sleep(1./fps)
+            c.close()
+            print 'wrote'
+
+        except socket.error:
+            pass
+        except KeyboardInterrupt:
+            break
+# c.close()
+sock.close()
+print 'Socket Closed' 
