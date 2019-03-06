b97903f33effa4d35ebd955c1c3cb64cb53e8ef7
nemaye <45652238+nemaye@users.noreply.github.com>
2019-03-06 16:23:29 +0530

    Update 4ultra.py

--- Yameen/4ultra.py
+++ Yameen/4ultra.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BCM)
 
@@ -9,7 +10,7 @@ trigger_right = 14
 echo_side_l = 19
 trigger_side_l = 26
 echo_side_r = 21
-trigger_side_l = 20
+trigger_side_r = 20
 
 
 GPIO.setup(trigger_left, GPIO.OUT)
@@ -24,45 +25,51 @@ GPIO.setup(echo_side_l, GPIO.IN)
 GPIO.setup(trigger_side_r, GPIO.OUT)
 GPIO.setup(echo_side_r, GPIO.IN)
 
-def Ultrasonic(pin):
-	GPIO.output(pin,False)
+def Ultrasonic(pin,pin2):
+	GPIO.output(pin2,False)
 	time.sleep(0.1)
 
-	GPIO.output(pin,True)
+	GPIO.output(pin2,True)
 	time.sleep(0.00001)
-	GPIO.output(pin,False)
-	while GPIO.input(pin) != GPIO.HIGH:
+	GPIO.output(pin2,False)
+
+	while GPIO.input(pin)==0:
 		pass
 	pulse_begins = time.time()
-	while GPIO.input(pin) == GPIO.HIGH:
+	
+	while GPIO.input(pin)==1:
 		pulse_stops = time.time()
-		pulse_duration = pulse_stops - pulse_begins
-		if pulse_duration > limit:
-			distance = pulse_duration * 34000/2
-			return distance,'alert'
-		return distance,'not alert'
+		if (pulse_stops-pulse_begins>0.004):
+			pulse_duration = pulse_stops-pulse_begins
+			distance = pulse_duration*34000/2
+			return distance,'not alert'
+			break
+	
+	pulse_duration = pulse_stops-pulse_begins
+	distance = pulse_duration*34000/2
+	return distance,'alert'
 
 while True:
-	dist_lf,alert_lf = Ultrasonic(echo_left)
-	dist_rf,alert_rf = Ultrasonic(echo_right)
-	dist_sl, alert_l = Ultrasonic(echo_side_l)
-	dist_sr, alert_r = Ultrasonic(echo_side_r)
+	dist_lf,alert_lf = Ultrasonic(echo_left,trigger_left)
+	dist_rf,alert_rf = Ultrasonic(echo_right,trigger_right)
+	dist_sl, alert_l = Ultrasonic(echo_side_l,trigger_side_l)
+	dist_sr, alert_r = Ultrasonic(echo_side_r,trigger_side_r)
 	#print(alert_r,alert_l,alert_rf,alert_lf)
-	if alert_rf == 'alert' and alert_lf == 'not alert':
-		print('turn left') 
-	elif alert_lf == 'alert' and alert_rf == 'not alert':
-		print('turn right')
- 	elif dist_lf > dist_rf:
- 		print('turn right')
- 	else:
- 		print('turn left')
+	if alert_rf == 'alert' or alert_lf == 'alert':
+		if dist_lf < dist_rf:
+ 			print('right')
+ 		else:
+ 			print('left')
+ 	
 
- 	if (alert_rf == 'not alert' and alert_lf == 'not alert'):
+ 	elif (alert_rf == 'not alert' and alert_lf == 'not alert'):
  		print('straight')
  		if alert_l == 'alert':
- 			print('cant turn left even if imu is saying')
- 			print('go straight until alert_l == not alert')
+ 			#rint('cant turn left even if imu is saying')
+ 			print('go straight')# until alert_l == not alert
 
  		elif alert_r == 'alert':
- 			print('cant turn right even if imu is saying')
- 			print('go straight until alert_r == not alert')
+ 			#rint('cant turn right even if imu is saying')
+ 			print('go straight')# until alert_r == not alert
+
+ 	
