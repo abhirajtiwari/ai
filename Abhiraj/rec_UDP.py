ef313d5384c4efedb7506dff6aa49569e4c21b63
Abhiraj Tiwari <abhirajtiwari@gmail.com>
2019-03-19 00:53:31 +0530

    yameen reciever as a function

---
+++ Abhiraj/rec_UDP.py
@@ -0,0 +1,27 @@
+import socket
+import numpy
+import time
+import cv2
+
+UDP_IP = "192.168.43.130"
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.bind ((UDP_IP, UDP_PORT))
+
+def getCameraFrame():
+    data, addr = sock.recvfrom(10000000)
+    print('inside')
+    frame = numpy.fromstring (data,dtype=numpy.uint8)
+    #print('frame shape',frame.shape)
+    #print('frame is',len(frame))
+    frame = numpy.transpose(frame)
+    frame = cv2.imdecode(frame, 1)
+    return frame
+
+if __name__ == '__main__':
+    while True:
+        cv2.imshow('frame', getCameraFrame())
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
