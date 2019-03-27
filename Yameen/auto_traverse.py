be6a061298777e804e1755843c9dba0f8ce26187
nemaye <45652238+nemaye@users.noreply.github.com>
2019-03-27 22:50:01 +0530

    Update auto_traverse.py

--- Yameen/auto_traverse.py
+++ Yameen/auto_traverse.py
@@ -7,8 +7,8 @@ import RPi.GPIO as GPIO
 
 add=0x1e
 
-lat2 = 13.3478
-lon2 = 74.79223
+#lat2 = 13.3478
+#lon2 = 74.79223
 #lat1=0.000000
 #lon1=0.000000
 #distance = 0.000000
@@ -24,6 +24,17 @@ adjustment=360
 x_manual=-0.2125
 y_manual=0.8025
 
+g = 0
+gate = input("Enter number of gates!!")
+lat=[]
+lon=[]
+for i in range(gate):
+	la = input('enter lat'+str(i)+':')
+	lo = input('enter lon'+str(i)+':')
+	lat.append(la)
+	lon.append(lo)
+
+
 i2c=smbus.SMBus(1)
 i2c.write_byte_data(add,0x20,0b01011100)#high performance
 i2c.write_byte_data(add,0x21,0b00000000)#4 gauss range
@@ -32,19 +43,14 @@ i2c.write_byte_data(add,0x23,0b00001000)
 
 GPIO.setmode(GPIO.BCM)
 
-echo_left = 26
-trigger_left = 19
-echo_right = 11
-trigger_right = 9
-echo_side_l = 16
-trigger_side_l = 20
-echo_side_r = 25
-trigger_side_r = 8
-
-ldir = 5
-rdir = 23
-lspeed = 13
-rspeed = 18
+echo_left = 11
+trigger_left = 9
+echo_right = 25
+trigger_right = 8
+echo_side_l = 26
+trigger_side_l = 19
+echo_side_r = 16
+trigger_side_r = 20
 
 min_x=0
 max_x=0
@@ -64,17 +70,6 @@ GPIO.setup(echo_side_l, GPIO.IN)
 
 GPIO.setup(trigger_side_r, GPIO.OUT)
 GPIO.setup(echo_side_r, GPIO.IN)
-
-
-GPIO.setup(ldir, GPIO.OUT)
-GPIO.setup(rdir,GPIO.OUT)
-GPIO.setup(lspeed, GPIO.OUT)
-GPIO.setup(rspeed,GPIO.OUT)
-
-pwm_l=GPIO.PWM(lspeed,100)
-pwm_r=GPIO.PWM(rspeed,100)
-pwm_l.start(50)
-pwm_r.start(50)
 
 
 def Ultrasonic(pin,pin2):
@@ -123,42 +118,28 @@ def Bearing(lat2,lon2):
 			if (lon1 == 'n/a'):
 				continue
 		# convert to radians
-			dLat = (lat2 - lat1) * m.pi / 180.0
-			dLon = (lon2 - lon1) * m.pi / 180.0
+			dLat = (lat[g] - lat1) * m.pi / 180.0
+			dLon = (lon[g] - lon1) * m.pi / 180.0
 
 		# convert to radians
 			lat1 = (lat1) * m.pi / 180.0
-			lat2 = (lat2) * m.pi / 180.0
+			lat[g] = (lat[g]) * m.pi / 180.0
 
-			y = m.cos(lat2) * m.sin(dLon)
-			x = (m.cos(lat1) * m.sin(lat2)) - (m.sin(lat1) * m.cos(lat2) * m.cos(dLon))
+			y = m.cos(lat[g]) * m.sin(dLon)
+			x = (m.cos(lat1) * m.sin(lat[g])) - (m.sin(lat1) * m.cos(lat[g]) * m.cos(dLon))
 			degree = m.atan2(y, x) * 180 / m.pi
 
 			if degree < 0:
 				degree += 360
 
 		# apply formulae
-			a = (pow(m.sin(dLat / 2), 2) + pow(m.sin(dLon / 2), 2) * m.cos(lat1) * m.cos(lat2))
+			a = (pow(m.sin(dLat / 2), 2) + pow(m.sin(dLon / 2), 2) * m.cos(lat1) * m.cos(lat[g]))
 			rad = 6378.1*1000
 			c = 2 * m.asin(m.sqrt(a))
 
 			distance = rad * c
 			print('dist',distance)
-			return distance, degree
-
-def state(st1,st2,st3,st4):
-	#lspeed.ChangeDutyCycle(50)
-	#rspeed.ChangeDutyCycle(50)
-	GPIO.output(ldir,st1)
-	GPIO.output(rdir,st3)
-	if (st1 != st3):
-		pwm_l.ChangeDutyCycle(50)
-		pwm_r.ChangeDutyCycle(50)
-	else:
-		pwm_l.ChangeDutyCycle(50)
-		pwm_r.ChangeDutyCycle(50)
-	#GPIO.output(lspeed,st2)
-	#GPIO.output(rspeed,st4)
+			return distance, degree,dist
 
 
 def LSM(min_x,max_x,min_y,max_y,min_z,max_z):
@@ -210,7 +191,7 @@ def LSM(min_x,max_x,min_y,max_y,min_z,max_z):
 		#print("HEADING is", heading)
 
 
-		dist, deg = Bearing(lat2, lon2)
+		dist, deg = Bearing(lat[g], lon[g])
 		print('head',heading)
 		print('deg',deg)
 
@@ -218,12 +199,9 @@ def LSM(min_x,max_x,min_y,max_y,min_z,max_z):
 	###########heading is the orientation of the imu from north
 		turn = heading - deg
 		
-		if dist<5:
-			print('destination reached')
-			state(0,0,0,0)
-			quit()
+
 		print('turn',turn)
-		return turn,heading
+		return turn,heading,dist
 
 
 
@@ -231,8 +209,17 @@ def LSM(min_x,max_x,min_y,max_y,min_z,max_z):
 while True:
 	#time.sleep(0.1)
 	try:
-		turning, head = LSM(min_x, max_x, min_y, max_y, min_z, min_z)
+		turning, head, doorie = LSM(min_x, max_x, min_y, max_y, min_z, min_z)
 
+		if doorie<5:
+			print('destination reached')
+			state(chr(5))
+			g=g+1
+			execfile('vidsend_UDP.py')
+			if g == gate:
+				quit()
+
+		
 		dist_lf,alert_lf = Ultrasonic(echo_left,trigger_left)
 		dist_rf,alert_rf = Ultrasonic(echo_right,trigger_right)
 		dist_sl, alert_l = Ultrasonic(echo_side_l,trigger_side_l)
@@ -240,13 +227,16 @@ while True:
 		
 		print(alert_l,alert_lf,alert_rf,alert_r)
 		
-		
+
+
+
+
 		if ((alert_lf == 'alert' and alert_rf == 'not alert') or (alert_lf == 'alert' and alert_rf == 'alert')):
-			turning, init_head = LSM(min_x, max_x, min_y, max_y, min_z, min_z)
+			turning, init_head, doorie = LSM(min_x, max_x, min_y, max_y, min_z, min_z)
 			while True:
 				print('Turn Right')
 				print(abs(head-init_head))
-				state(1,1,0,1)
+				state(chr(6))
 				turning, head = LSM(min_x, max_x, min_y, max_y, min_z, min_z)
 				if (abs(head-init_head)<75 or abs(head-init_head)>295):
 					continue
@@ -257,11 +247,11 @@ while True:
 				break
 
 		elif (alert_rf == 'alert' and alert_lf == 'not alert'):
-			turning, init_head = LSM(min_x, max_x, min_y, max_y, min_z, min_z)
+			turning, init_head, doorie = LSM(min_x, max_x, min_y, max_y, min_z, min_z)
 			while True:
 				print('Turn Left')
 				print(abs(head-init_head))
-				state(0,1,1,1)
+				state(chr(7))
 				turning, head = LSM(min_x, max_x, min_y, max_y, min_z, min_z)
 				if (abs(head-init_head)<75 or abs(head-init_head)>295):
 					continue
@@ -278,50 +268,61 @@ while True:
 				if((turning)<0 and (turning)<=-195):
 					print('anti-clock1', 360 + turning)
 					#print('turning',turning)
-					state(0,1,1,1)
+					state(chr(7))
+					continue
 				elif((turning)>15 and (turning)<165):
 					print('anti-clock2', turning)
 					#print('turning',turning)
-					state(0,1,1,1)
+					state(chr(7))
+					continue
 				else:
 					print('go straight in 1')
-					state(1,1,1,1)
+					state(chr(1))
+					continue
 			elif (alert_l == 'alert' and alert_r == 'not alert'):
 				if((turning)<-15 and (turning)>-165):
 					print('clock1', -turning)
 					#print('turning',turning)
-					state(1,1,0,1)
+					state(chr(6))
+					continue
 				elif((turning)>0 and (turning)>=195):
 					print('clock2', 360 - turning)
 					#print('turning',turning)
-					state(1,1,0,1)
+					state(chr(6))
+					continue
 				else:
 					print('go straight in 2')
-					state(1,1,1,1)
+					state(chr(1))
+					continue
 			elif (alert_r == 'not alert' and alert_l == 'not alert'):
 				if((turning)<0 and (turning)<=-195):
 					print('anti-clock1 in not', 360 + turning)
 					#print('turning',turning)
-					state(0,1,1,1)
+					state(chr(7))
+					continue
 				elif((turning)>15 and (turning)<165):
 					print('turning',  	turning)
 					print('anti-clock2 in not', turning)
 					#print('turning',turning)
-					state(0,1,1,1)
+					state(chr(7))
+					continue
 				elif((turning)<-15 and (turning)>-165):
 					print('clock1 in not', -turning)
 					#print('turning',turning)
-					state(1,1,0,1)
+					state(chr(6))
+					continue
 				elif((turning)>0 and (turning)>=195):
 					print('clock2 in not', 360 - turning)
 					#print('turning',turning)
-					state(1,1,0,1)
+					state(chr(6))
+					continue
 				else:
 					print('go straight in 3')
-					state(1,1,1,1)
+					state(chr(1))
 			else:
 				print('straight')
-				state(1,1,1,1)
+				state(chr(1))
+				continue
 	
 	#print('___________________________')
 
