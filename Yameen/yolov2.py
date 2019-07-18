7d15070e3d420689d81e040aa823a2add0f6a3fc
nemaye <45652238+nemaye@users.noreply.github.com>
2019-07-18 20:48:48 +0530

    Update yolov2.py

--- Yameen/yolov2.py
+++ Yameen/yolov2.py
@@ -1,28 +1,38 @@
 import cv2
 from darkflow.net.build import TFNet
-import matplotlib.pyplot as plt
+import numpy as np
+import time
 
-#%config InlineBackend.figure_format = 'svg'
-
-options = {
-		'model': 'cfg/yolo.cfg',
-		'load': 'bin/yolov2.weights',
-		'threshold': 0.3,
+option ={
+	'model': 'cfg/yolo.cfg',
+	'load': 'bin/yolov2.weights',
+	'threshold': 0.15,
 }
 
-tfnet = TFNet(options)
+tfnet = TFNet(option)
 
-img = cv2.imread('tennis.jpg',cv2.IMREAD_COLOR)
-result = tfnet.return_predict(img)
+capture = cv2.VideoCapture(0)
+colors = [tuple(255*np.random.rand(3)) for i in range(5)]
 
-print(result)
+while (capture.isOpened()):
+	stime = time.time()
+	ret, frame = capture.read()
+	results = tfnet.return_predict(frame)
+	if ret:
+		for color, result in zip(colors,results):
+			topL = (result['topleft']['x'],result['topleft']['y'])
+			bottomR = (result['bottomright']['x'],result['bottomright']['y'])
 
-topL = (result[0]['topleft']['x'],result[0]['topleft']['y'])
-bottomR = (result[0]['bottomright']['x'],result[0]['bottomright']['y'])
+			label = result['label']
 
-label = result[0]['label']
+			frame = cv2.rectangle(frame, topL, bottomR, color, 7)
+			frame = cv2.putText(frame, label,topL , cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
+		cv2.imshow('frame', frame)
+		print('FPS {:.1f}'.format(1/(time.time() - stime)))
+		if cv2.waitKey(1) & 0xFF ==ord('q'):
+			break
 
-img = cv2.rectangle(img, topL, bottomR, (0,255,255))
-img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-plt.imshow(img)
-plt.show()
+	else:
+		capture.release()
+		cv2.destroyAllWindows()
+		break
