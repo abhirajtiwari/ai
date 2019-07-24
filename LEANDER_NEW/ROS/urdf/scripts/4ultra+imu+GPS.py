8cde5ab314bc07c37a419e22ab9599dd537ccd77
leander-dsouza <45683974+leander-dsouza@users.noreply.github.com>
2019-07-24 23:54:18 +0530

    Update 4ultra+imu+GPS.py

--- LEANDER_NEW/ROS/urdf/scripts/4ultra+imu+GPS.py
+++ LEANDER_NEW/ROS/urdf/scripts/4ultra+imu+GPS.py
@@ -8,8 +8,9 @@ import time
 from sensor_msgs.msg import NavSatFix
 from math import *
 
+
 def arduino_map(x, in_min, in_max, out_min, out_max):
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 
 def haversine(lat1, lon1, lat2, lon2):
@@ -28,13 +29,9 @@ def haversine(lat1, lon1, lat2, lon2):
     a = (pow(sin(dLat / 2), 2) +
          pow(sin(dLon / 2), 2) *
          cos(lat1) * cos(lat2))
-    rad = 6378.1*1000
+    rad = 6378.1 * 1000
     c = 2 * asin(sqrt(a))
-    dist= rad*c
-    if dist<1:
-        brutestop()
-        quit()
-
+    dist = rad * c
 
 def bearing(lat1, lon1, lat2, lon2):
     global degree
@@ -49,20 +46,15 @@ def bearing(lat1, lon1, lat2, lon2):
         degree += 360
 
 
-lat2=49.89990654904074
-lon2=8.899935283587306
+lat2 = 49.89990654904074
+lon2 = 8.899935283587306
 
+pitch = 0.0
+roll = 0.0
+yaw = 0.0
 
-pitch=0.0
-roll=0.0
-yaw=0.0
-
-
-
-
-threshold=0.45
-b_threshold=0.2
-
+threshold = 0.45
+b_threshold = 0.2
 
 ob1 = Twist()
 
@@ -76,7 +68,7 @@ def short_angle(x, y):
 
 
 def forward():
-    #print("FORWARD")
+    # print("FORWARD")
     ob1.linear.x = 0.3
     ob1.linear.y = 0
     ob1.linear.z = 0
@@ -86,8 +78,9 @@ def forward():
     ob1.angular.z = 0
     pub.publish(ob1)
 
+
 def backward():
-    #print("BACKWARD")
+    # print("BACKWARD")
     ob1.linear.x = -0.3
     ob1.linear.y = 0
     ob1.linear.z = 0
@@ -98,8 +91,9 @@ def backward():
 
     pub.publish(ob1)
 
+
 def right():
-    #print("RIGHT")
+    # print("RIGHT")
     ob1.linear.x = 0
     ob1.linear.y = 0
     ob1.linear.z = 0
@@ -109,8 +103,9 @@ def right():
     ob1.angular.z = 2.5
     pub.publish(ob1)
 
+
 def left():
-    #print("LEFT")
+    # print("LEFT")
     ob1.linear.x = 0
     ob1.linear.y = 0
     ob1.linear.z = 0
@@ -122,87 +117,84 @@ def left():
 
 
 def brutestop():
-    #print("BRUTESTOP")
-    ob1.linear.x = 0
-    ob1.linear.y = 0
-    ob1.linear.z = 0
-
-    ob1.angular.x = 0
-    ob1.angular.y = 0
-    ob1.angular.z = 0
+    ob1 = Twist()
     pub.publish(ob1)
-
 
 
 def sideleftcheck():
     global distance4
 
-    if distance4 < threshold+0.3 :
+    if distance4 < threshold + 0.3:
         print("ULTRASONIC OVERRIDE: FORWARD---")
         forward()
         return -1
     else:
         return 1
+
 
 def siderightcheck():
     global distance3
 
-    if distance3 < threshold+0.3 :
+    if distance3 < threshold + 0.3:
         print("ULTRASONIC OVERRIDE: FORWARD---")
         forward()
         return -1
     else:
         return 1
 
-distance1=Range()
-distance2=Range()
-distance3=Range()
-distance4=Range()
 
+distance1 = Range()
+distance2 = Range()
+distance3 = Range()
+distance4 = Range()
 
-heading=0
-degree=0
-dist=0
+heading = 0
+degree = 0
+dist = 0
+
 
 def callback1(msg):
     global distance1
     distance1 = msg.range
-    #print("DISTANCE1=",distance1)
+    # print("DISTANCE1=",distance1)
+
 
 def callback2(msg):
     global distance2
-    distance2= msg.range
-    #print("DISTANCE2=", distance2)
+    distance2 = msg.range
+    # print("DISTANCE2=", distance2)
+
 
 def callback3(msg):
     global distance3
-    distance3= msg.range
-    #print("DISTANCE3=", distance3)
+    distance3 = msg.range
+    # print("DISTANCE3=", distance3)
+
 
 def callback4(msg):
     global distance4
-    distance4= msg.range
-    #print("DISTANCE4=", distance4)
+    distance4 = msg.range
+    # print("DISTANCE4=", distance4)
+
+
 #
 def callback_imu(msg):
     global heading
 
-    orientation_list=[msg.orientation.x,msg.orientation.y,msg.orientation.z,msg.orientation.w]
-    (roll,pitch,yaw)=euler_from_quaternion(orientation_list)
+    orientation_list = [msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w]
+    (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
 
-    heading=arduino_map(yaw,0,-3,0,180)
+    heading = arduino_map(yaw, 0, -3, 0, 180)
 
-    if heading<0:
-        heading+=360
-
+    if heading < 0:
+        heading += 360
 
 
 def callback_gps(msg):
-    global lat2,lon2,lat1,lon1
+    global lat2, lon2, lat1, lon1
 
-
-    lat1=msg.latitude
-    lon1=msg.longitude
+    lat1 = msg.latitude
+    lon1 = msg.longitude
 
     haversine(lat1, lon1, lat2, lon2)
     bearing(lat1, lon1, lat2, lon2)
@@ -246,12 +238,10 @@ def displaydata(t, dist):
         left()
 
 
-
 def ultrasonic():
+    global distance1, distance2, distance3, distance4, heading
 
-    global distance1,distance2,distance3,distance4,heading
-
-    #print("DISTANCE1=",distance1,"DISTANCE2=", distance2)
+    # print("DISTANCE1=",distance1,"DISTANCE2=", distance2)
 
     if distance1 < b_threshold or distance2 < b_threshold:
         backward()
@@ -266,16 +256,16 @@ def ultrasonic():
             while short_angle(headA, headB) < 90:
                 print("ULTRASONIC OVERRIDE: BACKWARD RIGHT")
                 right()
-                headB=heading
+                headB = heading
 
-            #brutestop()
+            # brutestop()
 
             checkB = sideleftcheck()
 
             while checkB == -1:
                 checkB = sideleftcheck()
 
-            return  -1
+            return -1
 
         else:
             print("ULTRASONIC OVERRIDE: BACKWARD LEFT")
@@ -288,7 +278,7 @@ def ultrasonic():
                 left()
                 headD = heading
 
-            #brutestop()
+            # brutestop()
             checkA = siderightcheck()
 
             while checkA == -1:
@@ -307,9 +297,8 @@ def ultrasonic():
             print("ULTRASONIC OVERRIDE: LEFT90")
             left()
             headB = heading
-           
 
-        #brutestop()
+        # brutestop()
         checkA = siderightcheck()
 
         while checkA == -1:
@@ -326,7 +315,7 @@ def ultrasonic():
             right()
             headD = heading
 
-        #brutestop()
+        # brutestop()
         checkB = sideleftcheck()
 
         while checkB == -1:
@@ -334,14 +323,14 @@ def ultrasonic():
 
         return -1
 
-    elif distance3 < threshold+0.3 :
+    elif distance3 < threshold + 0.3:
         print("ULTRASONIC OVERRIDE: FORWARD---")
         forward()
 
         return -1
 
 
-    elif distance4 < threshold+0.3 :
+    elif distance4 < threshold + 0.3:
         print("ULTRASONIC OVERRIDE: FORWARD---")
         forward()
 
@@ -350,13 +339,13 @@ def ultrasonic():
     else:
         return 1
 
-def listener():
 
+def listener():
     # In ROS, nodes are uniquely named. If two nodes with the same
-       # name are launched, the previous one is kicked off. The
-       # anonymous=True flag means that rospy will choose a unique
-       # name for our 'listener' node so that multiple listeners can
-       # run simultaneously.
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
     rospy.Subscriber("champ1", Range, callback1)
     rospy.Subscriber("champ2", Range, callback2)
     rospy.Subscriber("champ3", Range, callback3)
@@ -367,37 +356,36 @@ def listener():
 
     while not rospy.is_shutdown():
 
-        #print("DISTANCE1=", distance1)
-        #print("DISTANCE2=", distance2)
-        while True:
-            rospy.sleep(0.01)
-            v=ultrasonic()
-            if v==1:
-                pass
-            else:
-                continue
+        # print("DISTANCE1=", distance1)
+        # print("DISTANCE2=", distance2)
 
+        rospy.sleep(0.01)
+        v = ultrasonic()
+        if v == 1:
+            pass
+        else:
+            continue
 
-            t= heading-degree
+        t = heading - degree
+        if dist<1:
+            brutestop()
+            break
 
-            displaydata(t,dist)
-
-
-
+        displaydata(t, dist)
 
     # spin() simply keeps python from exiting until this node is stopped
+
 
 if __name__ == '__main__':
     try:
         global ob1
         pub = rospy.Publisher('champ', Twist, queue_size=10)
         rospy.init_node('Communication', anonymous=True)
-        rate = rospy.Rate(1)  # 1hz
+        rate = rospy.Rate(50)  # 1hz
 
-        rospy.loginfo(ob1)
+
 
         listener()
 
     except rospy.ROSInterruptException:
         pass
-
